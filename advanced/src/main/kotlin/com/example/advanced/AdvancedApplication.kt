0ca477dfa3974b7e4d3eb78c830f8f8b7661ccc1
hyune-c <choi8608@gmail.com>
2022-10-16 19:22:24 +0900

    :wrench: [advanced] init

---
+++ advanced/src/main/kotlin/com/example/advanced/AdvancedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.advanced
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdvancedApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdvancedApplication>(*args)
+}
