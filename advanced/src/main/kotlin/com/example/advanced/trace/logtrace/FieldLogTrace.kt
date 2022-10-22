73075b42d5fce2b67a6df0a2614677b0b60280a6
hyune-c <choi8608@gmail.com>
2022-10-23 04:15:01 +0900

    :sparkles: [advanced] add fieldlogtrace implementation

---
+++ advanced/src/main/kotlin/com/example/advanced/trace/logtrace/FieldLogTrace.kt
@@ -0,0 +1,69 @@
+package com.example.advanced.trace.logtrace
+
+import com.example.advanced.trace.TraceId
+import com.example.advanced.trace.TraceStatus
+import com.example.advanced.trace.hellotrace.HelloTraceV2
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class FieldLogTrace : LogTrace {
+
+    private var traceIdHolder: TraceId? = null;
+
+    override fun begin(message: String?): TraceStatus {
+        syncTraceId()
+
+        val traceId = traceIdHolder!!
+        val startTimeMs = System.currentTimeMillis()
+        val content = addSpace(START_PREFIX, traceId.level)
+        HelloTraceV2.log.info("[${traceId.id}] ${content}${message}")
+
+        return TraceStatus(traceId, startTimeMs, message)
+    }
+
+    override fun end(status: TraceStatus) = complete(status, null)
+    override fun exception(status: TraceStatus, e: Exception) = complete(status, e)
+
+    private fun syncTraceId() {
+        traceIdHolder = traceIdHolder?.createNextId() ?: TraceId()
+    }
+
+    private fun complete(status: TraceStatus, e: Exception?) {
+        val stopTimeMs = System.currentTimeMillis()
+        val resultTimeMs = stopTimeMs - status.startTimeMs
+        val traceId = status.traceId
+
+        if (e == null) {
+            val content = addSpace(COMPLETE_PREFIX, traceId.level)
+            HelloTraceV2.log.info("[${traceId.id}] ${content}${status.message} time=${resultTimeMs}ms")
+        } else {
+            val content = addSpace(EX_PREFIX, traceId.level)
+            HelloTraceV2.log.info("[${traceId.id}] ${content}${status.message} time=${resultTimeMs}ms ex=${e.toString()}")
+        }
+
+        releaseTraceId()
+    }
+
+    private fun releaseTraceId() {
+        traceIdHolder = if (traceIdHolder!!.isFirstLevel()) {
+            null
+        } else {
+            traceIdHolder!!.createPreviousId()
+        }
+    }
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+        private const val START_PREFIX = "-->"
+        private const val COMPLETE_PREFIX = "<--"
+        private const val EX_PREFIX = "<X-"
+        private fun addSpace(prefix: String, level: Int): String {
+            val sb = StringBuilder()
+            for (i in 0 until level) {
+                sb.append(if (i == level - 1) "|$prefix" else "|   ")
+            }
+            return sb.toString()
+        }
+    }
+}
