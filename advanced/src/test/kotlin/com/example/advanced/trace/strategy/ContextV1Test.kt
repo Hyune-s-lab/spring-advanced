15715d152b2d5c54a18dbffcb7840a766494c9ec
hyune-c <choi8608@gmail.com>
2022-10-26 18:38:46 +0900

    :white_check_mark: [advanced] ContextV1Test

---
+++ advanced/src/test/kotlin/com/example/advanced/trace/strategy/ContextV1Test.kt
@@ -0,0 +1,37 @@
+package com.example.advanced.trace.strategy
+
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class ContextV1Test {
+    @Test
+    fun strategyV0() {
+        logic1()
+        logic2()
+    }
+
+    private fun logic1() {
+        val startTime = System.currentTimeMillis()
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행")
+        //비즈니스 로직 종료
+        val endTime = System.currentTimeMillis()
+        val resultTime = endTime - startTime
+        log.info("resultTime=${resultTime}")
+    }
+
+    private fun logic2() {
+        val startTime = System.currentTimeMillis()
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행")
+        //비즈니스 로직 종료
+        val endTime = System.currentTimeMillis()
+        val resultTime = endTime - startTime
+        log.info("resultTime=${resultTime}")
+    }
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(this::class.java)
+    }
+}
